c68f261499e5d162e4d3d1f21aa2c3cdc4886e45
ApolloPharmacy <gopalakrishna_dev@apollopharmacy.org>
2020-02-12 17:45:44 +0530

    Merge branch 'master' of https://github.com/thresholdsoft/apolloPharmacist into add_item_cart
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/apollopharmacy/mpospharmacist/data/db/model/Cart.java
@@ -0,0 +1,42 @@
+package com.apollopharmacy.mpospharmacist.data.db.model;
+
+import com.apollopharmacy.mpospharmacist.ui.searchproductlistactivity.model.GetItemDetailsRes;
+
+import java.util.ArrayList;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Cart extends RealmObject {
+
+    @PrimaryKey
+    private int id;
+
+    private String transactionId;
+    private RealmList<CartItems> itemsArrayList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public RealmList<CartItems> getItemsArrayList() {
+        return itemsArrayList;
+    }
+
+    public void setItemsArrayList(RealmList<CartItems> itemsArrayList) {
+        this.itemsArrayList = itemsArrayList;
+    }
+}
