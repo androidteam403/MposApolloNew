f627cbb506f0545ede81fdd5a3628ef87fca55cd
ThresholdAndroidTeam <android@thresholdsoft.com>
2020-03-04 15:12:42 +0530

    Order Summary View Updated

---
+++ app/src/main/java/com/apollopharmacy/mpospharmacist/ui/ordersummary/model/PaidAmountBean.java
@@ -0,0 +1,31 @@
+package com.apollopharmacy.mpospharmacist.ui.ordersummary.model;
+
+public class PaidAmountBean {
+    private String paymentMode;
+    private String paymentInfo;
+    private String paidAmount;
+
+    public String getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(String paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+
+    public String getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    public void setPaymentInfo(String paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+
+    public String getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(String paidAmount) {
+        this.paidAmount = paidAmount;
+    }
+}
