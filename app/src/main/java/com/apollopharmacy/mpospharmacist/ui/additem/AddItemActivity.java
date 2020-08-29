a53ba937740972aa47cf053c808b8ad6f2e9f62e
androidteam403 <android@thresholdsoft.com>
2020-08-29 20:09:28 +0530

    merged files

--- app/src/main/java/com/apollopharmacy/mpospharmacist/ui/additem/AddItemActivity.java
+++ app/src/main/java/com/apollopharmacy/mpospharmacist/ui/additem/AddItemActivity.java
@@ -976,7 +976,7 @@ public class AddItemActivity extends BaseActivity implements AddItemMvpView, Cus
     }
 
     CalculatePosTransactionRes calculatePosTransactionRes;
-String data="8";
+    String data="8";
     @Override
     public void onSuccessCalculatePosTransaction(CalculatePosTransactionRes posTransactionRes) {
         posTransactionRes.setSalesOrigin(saleslistData);
@@ -1934,15 +1934,10 @@ String data="8";
                         addItemBinding.cardPaymentAmountEditText.setSelection(addItemBinding.cardPaymentAmountEditText.getText().length());
                     }
 //                    else if (text.indexOf("0") == 0) {
-<<<<<<< HEAD
 //                        addItemBinding.cardPaymentAmountEditText.setText(addItemBinding.cardPaymentAmountEditText.getText().toString().replace("0", ""));
-=======
-//                        digitEditText.setText(digitEditText.getText().toString().replace("0", ""));
->>>>>>> 65031642be133590b0b8d130c1cbb3d88c88eece
 //                    }
                 } else {
-                    if (text.contains(".") &&
-                            text.indexOf(".") != text.length() - 1 &&
+                    if (text.contains(".") && text.indexOf(".") != text.length() - 1 &&
                             String.valueOf(text.charAt(text.length() - 1)).equals(".")) {
                         addItemBinding.cardPaymentAmountEditText.setText(text.substring(0, text.length() - 1));
                         addItemBinding.cardPaymentAmountEditText.setSelection(addItemBinding.cardPaymentAmountEditText.getText().length());
@@ -1961,16 +1956,25 @@ String data="8";
                             }
                         }
                     }
+                    if (!text.contains(".")) {
+                        if (!TextUtils.isEmpty(text) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCardMode()) {
+                            if (!mPresenter.validTenderLimit(Double.parseDouble(text), "card")) {
+                                addItemBinding.cardPaymentAmountEditText.setText("");
+                            }
+                        }
+                    }
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-//                if (!TextUtils.isEmpty(editable) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCardMode()) {
-//                    if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "card")) {
-//                        addItemBinding.cardPaymentAmountEditText.setText("");
-//                    }
-//                }
+                if (!editable.toString().isEmpty() && editable.toString().length() > 1) {
+                    if (!TextUtils.isEmpty(editable) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCardMode()) {
+                        if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "card")) {
+                            addItemBinding.cardPaymentAmountEditText.setText("");
+                        }
+                    }
+                }
             }
         });
 
@@ -1989,15 +1993,10 @@ String data="8";
                         addItemBinding.cashPaymentAmountEdit.setSelection(addItemBinding.cashPaymentAmountEdit.getText().length());
                     }
 //                    else if (text.indexOf("0") == 0) {
-<<<<<<< HEAD
 //                        addItemBinding.cashPaymentAmountEdit.setText(addItemBinding.cashPaymentAmountEdit.getText().toString().replace("0", ""));
-=======
-//                        digitEditText.setText(digitEditText.getText().toString().replace("0", ""));
->>>>>>> 65031642be133590b0b8d130c1cbb3d88c88eece
 //                    }
                 } else {
-                    if (text.contains(".") &&
-                            text.indexOf(".") != text.length() - 1 &&
+                    if (text.contains(".") && text.indexOf(".") != text.length() - 1 &&
                             String.valueOf(text.charAt(text.length() - 1)).equals(".")) {
                         addItemBinding.cashPaymentAmountEdit.setText(text.substring(0, text.length() - 1));
                         addItemBinding.cashPaymentAmountEdit.setSelection(addItemBinding.cashPaymentAmountEdit.getText().length());
@@ -2016,16 +2015,25 @@ String data="8";
                             }
                         }
                     }
+                    if (!text.contains(".")) {
+                        if (!TextUtils.isEmpty(text) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCashMode()) {
+                            if (!mPresenter.validTenderLimit(Double.parseDouble(text), "Cash")) {
+                                addItemBinding.cashPaymentAmountEdit.setText("");
+                            }
+                        }
+                    }
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-//                if (!TextUtils.isEmpty(editable) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCashMode()) {
-//                    if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "Cash")) {
-//                        addItemBinding.cashPaymentAmountEdit.setText("");
-//                    }
-//                }
+                if (!editable.toString().isEmpty() && editable.toString().length() > 1) {
+                    if (addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCashMode()) {
+                        if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "Cash")) {
+                            addItemBinding.cashPaymentAmountEdit.setText("");
+                        }
+                    }
+                }
             }
         });
 
@@ -2042,13 +2050,11 @@ String data="8";
                     if (text.contains(".") && text.indexOf(".") == 0) {
                         addItemBinding.creditPaymentAmountEdit.setText(addItemBinding.creditPaymentAmountEdit.getText().toString().replace(".", ""));
                         addItemBinding.creditPaymentAmountEdit.setSelection(addItemBinding.creditPaymentAmountEdit.getText().length());
+                    } else if (text.indexOf("0") == 0) {
+                        addItemBinding.creditPaymentAmountEdit.setText(addItemBinding.creditPaymentAmountEdit.getText().toString().replace("0", ""));
                     }
-//                    else if (text.indexOf("0") == 0) {
-//                        digitEditText.setText(digitEditText.getText().toString().replace("0", ""));
-//                    }
                 } else {
-                    if (text.contains(".") &&
-                            text.indexOf(".") != text.length() - 1 &&
+                    if (text.contains(".") && text.indexOf(".") != text.length() - 1 &&
                             String.valueOf(text.charAt(text.length() - 1)).equals(".")) {
                         addItemBinding.creditPaymentAmountEdit.setText(text.substring(0, text.length() - 1));
                         addItemBinding.creditPaymentAmountEdit.setSelection(addItemBinding.creditPaymentAmountEdit.getText().length());
@@ -2067,16 +2073,25 @@ String data="8";
                             }
                         }
                     }
+                    if (!text.contains(".")) {
+                        if (!TextUtils.isEmpty(text) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCreditMode()) {
+                            if (!mPresenter.validTenderLimit(Double.parseDouble(text), "credit")) {
+                                addItemBinding.creditPaymentAmountEdit.setText("");
+                            }
+                        }
+                    }
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-//                if (!TextUtils.isEmpty(editable) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCreditMode()) {
-//                    if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "credit")) {
-//                        addItemBinding.creditPaymentAmountEdit.setText("");
-//                    }
-//                }
+                if (!editable.toString().isEmpty() && editable.toString().length() > 1) {
+                    if (!TextUtils.isEmpty(editable) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isCreditMode()) {
+                        if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "credit")) {
+                            addItemBinding.creditPaymentAmountEdit.setText("");
+                        }
+                    }
+                }
             }
         });
 
@@ -2093,13 +2108,11 @@ String data="8";
                     if (text.contains(".") && text.indexOf(".") == 0) {
                         addItemBinding.oneApolloAmountEditText.setText(addItemBinding.oneApolloAmountEditText.getText().toString().replace(".", ""));
                         addItemBinding.oneApolloAmountEditText.setSelection(addItemBinding.oneApolloAmountEditText.getText().length());
+                    } else if (text.indexOf("0") == 0) {
+                        addItemBinding.oneApolloAmountEditText.setText(addItemBinding.oneApolloAmountEditText.getText().toString().replace("0", ""));
                     }
-//                    else if (text.indexOf("0") == 0) {
-//                        digitEditText.setText(digitEditText.getText().toString().replace("0", ""));
-//                    }
                 } else {
-                    if (text.contains(".") &&
-                            text.indexOf(".") != text.length() - 1 &&
+                    if (text.contains(".") && text.indexOf(".") != text.length() - 1 &&
                             String.valueOf(text.charAt(text.length() - 1)).equals(".")) {
                         addItemBinding.oneApolloAmountEditText.setText(text.substring(0, text.length() - 1));
                         addItemBinding.oneApolloAmountEditText.setSelection(addItemBinding.oneApolloAmountEditText.getText().length());
@@ -2118,16 +2131,25 @@ String data="8";
                             }
                         }
                     }
+                    if (!text.contains(".")) {
+                        if (!TextUtils.isEmpty(text) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isOneApolloMode()) {
+                            if (!mPresenter.validTenderLimit(Double.parseDouble(text), "gift")) {
+                                addItemBinding.oneApolloAmountEditText.setText("");
+                            }
+                        }
+                    }
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-//                if (!TextUtils.isEmpty(editable) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isOneApolloMode()) {
-//                    if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "gift")) {
-//                        addItemBinding.oneApolloAmountEditText.setText("");
-//                    }
-//                }
+                if (!editable.toString().isEmpty() && editable.toString().length() > 1) {
+                    if (!TextUtils.isEmpty(editable) && addItemBinding.getIsPaymentMode() != null && addItemBinding.getIsPaymentMode() && paymentMethodModel.isOneApolloMode()) {
+                        if (!mPresenter.validTenderLimit(Double.parseDouble(editable.toString()), "gift")) {
+                            addItemBinding.oneApolloAmountEditText.setText("");
+                        }
+                    }
+                }
             }
         });
     }
