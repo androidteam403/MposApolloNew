95c8217eb3782abe3566a9e73ef52d87359b48ca
HP <abc@gmail.com>
2023-11-10 10:38:08 +0530

    .

--- app/src/main/java/com/apollopharmacy/mpospharmacistTest/ui/pbas/batchlist/batchlistscanner/BatchlistScannerMvpView.java
+++ app/src/main/java/com/apollopharmacy/mpospharmacistTest/ui/pbas/batchlist/batchlistscanner/BatchlistScannerMvpView.java
@@ -2,6 +2,9 @@ package com.apollopharmacy.mpospharmacistTest.ui.pbas.batchlist.batchlistscanner
 
 import com.apollopharmacy.mpospharmacistTest.ui.base.MvpView;
 import com.apollopharmacy.mpospharmacistTest.ui.batchonfo.model.GetBatchInfoRes;
+import com.apollopharmacy.mpospharmacistTest.ui.pbas.batchlist.batchlistscanner.model.ReasonListResponse;
+
+import java.util.List;
 
 public interface BatchlistScannerMvpView extends MvpView {
     void onClickClose();
@@ -13,4 +16,6 @@ public interface BatchlistScannerMvpView extends MvpView {
     void onClickReferToAdmin();
 
     void onClickOnHold();
+
+    void onSuccessGetResonListApiCall(ReasonListResponse reasonListResponse);
 }
